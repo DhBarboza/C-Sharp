a5f17139757138a110210b41c58dce02b1f5adf8
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-05-28 00:12:28 -0300

    Return to learning

---
+++ CSharpLearning/Fundamentals/1-FirstProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLearning.Fundamentals {
+    class _1_FirstProgram {
+        static void Run() {
+            Console.WriteLine("First Testing");
+        }
+    }
+}
