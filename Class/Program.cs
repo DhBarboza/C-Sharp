83cbf990029ff43f89d3ef8794387ddc0ce90bce
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-08-25 14:25:26 -0300

    Config runner

---
+++ Class/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Class.Heritage;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
